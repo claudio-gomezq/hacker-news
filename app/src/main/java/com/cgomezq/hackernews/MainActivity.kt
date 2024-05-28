2171df2509cf614675cafd44031e728952e4d709
claudio-gomezq <claudio.gomezquidel@gmail.com>
2024-05-28 14:23:14 -0400

    project created

---
+++ app/src/main/java/com/cgomezq/hackernews/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.cgomezq.hackernews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.cgomezq.hackernews.ui.theme.HackerNewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HackerNewsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                    }
+                }
+            }
+        }
+    }
+}
